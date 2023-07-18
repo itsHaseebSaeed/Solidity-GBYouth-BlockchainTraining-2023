20b820de0bf3c2312fe41cb9ac620a1c81765f49
Shan-barcha <shanbarcha11@gmil.com>
2023-07-18 14:46:52 +0500

    upgrade state variable

--- Lab-01/stateVariable.sol
+++ Lab-01/stateVariable.sol
@@ -1,11 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-
-contract stateVariable {
-    uint public storedData ;
-
-    constructor (){
-        storedData = 10;
-    }
-
+contract SimpleStorage {
+// State variable to store a number
+uint public num;
+function input(uint _num) public {
+num = _num;
 }
+function output() public view returns (uint) {
+return num;
+}
+}
+// pragma solidity ^0.8.17;
+
+// contract stateVariable {
+//     uint public storedData ;
+
+//     constructor (){
+//         storedData = 10;
+//     }
+
+// }
