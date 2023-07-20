60ce6a7e4287ec7d504130ab9eaf967d8b54d68d
Shan-barcha <shanbarcha11@gmil.com>
2023-07-20 20:06:17 +0500

    constants & immuiatable concepts

--- variables/immuiatable.sol
+++ variables/immuiatable.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.17;
 
 contract WalletAddress {
+    // Define an immutable public variable 'ADMIN_Address' to store the administrator's Ethereum address
     address public immutable ADMIN_Address = 0x41F00738D8bB3372905dEa414F07B2BfED248329;
 
+    // Function to get the administrator's Ethereum address
     function getAdminAddress() public view returns (address) {
+        // Return the stored 'ADMIN_Address' variable, representing the administrator's address
         return ADMIN_Address;
     }
 }
--- variables/nameConcat.sol
+++ variables/nameConcat.sol
@@ -1,16 +1,23 @@
-// // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+// Contract 'person' to represent a person with first and last names.
 contract person {
-    string public firstName ;
+    // Declare two public state variables to store the first and last names of the person.
+    string public firstName;
     string public lastName;
 
-    function setFulName(string memory __firstName , string memory __lastName) public {
-        firstName = __firstName ;
-        lastName =  __lastName;
+    // Function to set the full name of the person.
+    function setFullName(string memory __firstName, string memory __lastName) public {
+        // Update the 'firstName' and 'lastName' variables with the provided values.
+        firstName = __firstName;
+        lastName = __lastName;
     }
-    function getFullName () public view returns (string memory){
-        return  string(abi.encodePacked(firstName ," " ,lastName));
+
+    // Function to get the full name of the person.
+    function getFullName() public view returns (string memory) {
+        // Concatenate the 'firstName' and 'lastName' variables and return the full name.
+        return string(abi.encodePacked(firstName, " ", lastName));
     }
 }
 
